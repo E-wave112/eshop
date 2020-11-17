9dbba1b2a5ae73dbe40e6e4c46b1d887a3146a57
e-wave <osagieiyayi09@gmail.com>
2020-11-17 02:47:35 +0000

    heroku

--- orders/admin.py
+++ orders/admin.py
@@ -49,6 +49,6 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
     'address', 'postal_code', 'city', 'paid',
     'created', 'updated']
-    list_filter = ['paid', 'created', 'updated',]
+    list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
