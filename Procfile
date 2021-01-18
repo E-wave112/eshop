a323e17713d81b9c345b78d4f04737a62b1a777c
E-wave112 <osagieiyayi09@gmail.com>
2021-01-18 21:15:54 +0000

    added appropriate celery workers

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
 web: gunicorn eshop.wsgi --log-file -
+worker: celery worker --app=tasks.app
+beat: celery beat --app=tasks.app
--- eshop/celery.py
+++ eshop/celery.py
@@ -1,8 +1,9 @@
 import os
+from decouple import config
 from celery import Celery
 # set the default Django settings module for the 'celery' program.
-redis_url='redis://localhost:6379'
-redis_url='redis://localhost:6379'
+redis_url=config('BROKER_URL')
+redis_url=config('BROKER_URL')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eshop.settings')
 app = Celery('eshop',broker_pool_limit=1, broker=redis_url,result_backend=redis_url)
 app.config_from_object('django.conf:settings', namespace='CELERY')
