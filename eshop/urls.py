bb13da59eeb34d895cfc020082150d41def65378
E-wave112 <osagieiyayi09@gmail.com>
2021-08-21 11:58:13 +0000

    changed the debug mode

--- eshop/urls.py
+++ eshop/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('',include('shop.urls',namespace='shop')),
     path('admin/', admin.site.urls),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
