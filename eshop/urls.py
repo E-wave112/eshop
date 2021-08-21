25d6e66f93f5e657ae0ccdb27464cf20b71a3389
E-wave112 <osagieiyayi09@gmail.com>
2021-08-21 05:05:50 +0000

    edited the root urls file

--- eshop/urls.py
+++ eshop/urls.py
@@ -24,6 +24,4 @@ urlpatterns = [
      path('payment/', include('payment.urls', namespace='payment')),
     path('',include('shop.urls',namespace='shop')),
     path('admin/', admin.site.urls),
-]
-if settings:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
